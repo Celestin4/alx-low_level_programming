1a02969bf74ebf85c0dce377cc217427822f04bc
Celestin4 <nteziryayocelestin3@gmail.com>
2023-04-10 18:08:52 +0200

    file io tasks

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,26 +8,26 @@
  * Return: If the function fails - -1.
  *         Otherwise - 1.
  */
-int create_file(const char *filename, char *text_content)
+int create_file(const char *new_filename, char *new_text_content)
 {
-	int o, w, len = 0;
+    int fd, w, len = 0;
 
-	if (filename == NULL)
-		return (-1);
+    if (new_filename == NULL)
+        return (-1);
 
-	if (text_content != NULL)
-	{
-		for (len = 0; text_content[len];)
-			len++;
-	}
+    if (new_text_content != NULL)
+    {
+        for (len = 0; new_text_content[len];)
+            len++;
+    }
 
-	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	w = write(o, text_content, len);
+    fd = open(new_filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+    w = write(fd, new_text_content, len);
 
-	if (o == -1 || w == -1)
-		return (-1);
+    if (fd == -1 || w == -1)
+        return (-1);
 
-	close(o);
+    close(fd);
 
-	return (1);
+    return (1);
 }
