bf67179a429c442436aa4d47b85687543b79d944
Celestin4 <nteziryayocelestin3@gmail.com>
2023-07-10 19:18:56 +0200

    bit manipulation project

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,33 +1,30 @@
-#include "3-calc.h"
-
-/**
- * get_op_func - function pointer that selects the correct function to perform
- * the operation asked by the user
- * @s: the operator given by the user
- *
- * Return: pointer to the function that corresponds to the
- * operator given as a parameter
- */
-int (*get_op_func(char *s))(int, int)
+#include <stdio.h>
+int calculateLength(char*);
+ 
+void main() 
 {
-	op_t ops[] = {
-		{"+", op_add},
-		{"-", op_sub},
-		{"*", op_mul},
-		{"/", op_div},
-		{"%", op_mod},
-		{NULL, NULL}
-	};
-	int i;
+   char str1[25];
+   int l;
+	printf("\n\n Pointer : Calculate the length of the string :\n"); 
+	printf("---------------------------------------------------\n");
+   
+   printf(" Input a string : ");
+   fgets(str1, sizeof str1, stdin);
+   
+   l = calculateLength(str1);
+   printf(" The length of the given string %s is : %d ", str1, l-1);
+   printf("\n\n");
 
-	i = 0;
-
-	while (ops[i].op)
-	{
-		if (strcmp(ops[i].op, s) == 0)
-			return (ops[i].f);
-		i++;
-	}
-
-	return (NULL);
 }
+ 
+int calculateLength(char* ch) // ch = base address of array str1 ( &str1[0]  )
+{
+   int ctr = 0;
+   while (*ch != '\0') 
+   {
+      ctr++;
+      ch++;
+   }
+   return ctr;
+}
+
---
+++ 0x14-bit_manipulation/README.md
@@ -0,0 +1,3 @@
+0x14. C - Bit manipulation
+
+This project is intended with helping students to be familiar with concept of bit manipulation in c.
